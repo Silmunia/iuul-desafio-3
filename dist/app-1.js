c8888561e7b30e5b93a6ad5cb5550c4eb5411c4b
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-04-06 18:07:47 -0300

    Add files for Application 1

---
+++ dist/app-1.js
@@ -0,0 +1,27 @@
+"use strict";
+class Pessoa {
+    constructor(cpf, nome, telefone) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+}
+class Cargo {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+class Funcionario extends Pessoa {
+    constructor(nomeDoCargo, cpf, nome, telefone, salario) {
+        super(cpf, nome, telefone);
+        this.cargo = new Cargo(nomeDoCargo);
+        this.salario = salario;
+    }
+    autenticar() {
+        return true;
+    }
+}
+let atendente = new Funcionario("Atendente", "123.456.789-10", "Fun A", "(12) 3456-7890", 2500);
+let gerente = new Funcionario("Gerente", "109.876.543-21", "Fun B", "(10) 9876-5432", 5000);
+console.log(atendente);
+console.log(gerente);
