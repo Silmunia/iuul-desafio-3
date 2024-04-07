a1931c22cabcc9361d4ef578dc271adcc032e99e
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-04-07 12:29:40 -0300

    Refactor App 4 to use new Cliente methods

--- src/apps/app-4.ts
+++ src/apps/app-4.ts
@@ -6,20 +6,16 @@ import Cliente from "../objects/classes/Cliente";
 
 let clienteA = new Cliente("123.456.789-10", "Cli A", "(12) 3456-7890", true, new Endereco("12345-67", "Log A", "123", "Apartamento 456", "Cidade A", "UF B"), new ContaCorrente(123, 100));
 
-clienteA.contas[0].depositar(1000);
+clienteA.fazerDeposito(123, 1000);
 
 let clienteB = new Cliente("456.123.789-10", "Cli B", "(10) 1256-7889", true, new Endereco("12398-66", "Log A", "13", "Apartamento 404", "Cidade A", "UF B"), new ContaPoupanca(456));
 
-clienteB.contas[0].depositar(1000);
+clienteB.fazerDeposito(456, 1000);
 
 if (clienteA.contas[0] instanceof ContaCorrente) {
     clienteA.contas[0].transferir(clienteB.contas[0], 500);
 }
 
-if (clienteA.contas[0] instanceof ContaCorrente) {
-    console.log(clienteA.contas[0].calcularSaldo());
-}
+console.log(clienteA.calcularSaldoDeConta(123));
 
-if (clienteB.contas[0] instanceof ContaPoupanca) {
-    console.log(clienteB.contas[0].calcularSaldo());
-}
+console.log(clienteB.calcularSaldoDeConta(456));
