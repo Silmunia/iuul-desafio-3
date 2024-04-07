c6e860f5afaab25c3d33fa91741f35d17087bc31
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-04-07 12:00:03 -0300

    Refactor App 3 to use new methods

--- src/apps/app-3.ts
+++ src/apps/app-3.ts
@@ -5,10 +5,10 @@ import Cliente from "../objects/classes/Cliente";
 
 let cliente = new Cliente("123.456.789-10", "Cli A", "(12) 3456-7890", true, new Endereco("12345-67", "Log A", "123", "Apartamento 456", "Cidade A", "UF B"), new ContaCorrente(123, 100));
 
-cliente.contas[0].depositar(100);
-cliente.contas[0].depositar(100);
-cliente.contas[0].depositar(100);
+cliente.fazerDeposito(123, 100);
+cliente.fazerDeposito(123, 100);
+cliente.fazerDeposito(123, 100);
 
-cliente.contas[0].sacar(50);
+cliente.fazerSaque(123, 50);
 
 console.log(cliente.calcularSaldoDeConta(123));
