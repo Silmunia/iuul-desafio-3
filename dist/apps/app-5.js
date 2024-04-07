236597c0fc7100dd2d5b2e48518598f12cf6a576
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-04-07 12:44:25 -0300

    Refactor App 5 to use new methods

--- dist/apps/app-5.js
+++ dist/apps/app-5.js
@@ -8,15 +8,9 @@ const ContaCorrente_1 = __importDefault(require("../objects/classes/ContaCorrent
 const ContaPoupanca_1 = __importDefault(require("../objects/classes/ContaPoupanca"));
 const Cliente_1 = __importDefault(require("../objects/classes/Cliente"));
 let clienteA = new Cliente_1.default("123.456.789-10", "Cli A", "(12) 3456-7890", true, new Endereco_1.default("12345-67", "Log A", "123", "Apartamento 456", "Cidade A", "UF B"), new ContaCorrente_1.default(123, 100));
-clienteA.contas[0].depositar(300);
+clienteA.fazerDeposito(123, 300);
 let clienteB = new Cliente_1.default("456.123.789-10", "Cli B", "(10) 1256-7889", true, new Endereco_1.default("12398-66", "Log A", "13", "Apartamento 404", "Cidade A", "UF B"), new ContaPoupanca_1.default(456));
-clienteB.contas[0].depositar(100);
-if (clienteA.contas[0] instanceof ContaCorrente_1.default) {
-    clienteA.contas[0].transferir(clienteB.contas[0], 1000);
-}
-if (clienteA.contas[0] instanceof ContaCorrente_1.default) {
-    console.log(clienteA.contas[0].calcularSaldo());
-}
-if (clienteB.contas[0] instanceof ContaPoupanca_1.default) {
-    console.log(clienteB.contas[0].calcularSaldo());
-}
+clienteB.fazerDeposito(456, 100);
+clienteA.fazerTransferencia(123, clienteB, 456, 1000);
+console.log(clienteA.calcularSaldoDeConta(123));
+console.log(clienteB.calcularSaldoDeConta(456));
