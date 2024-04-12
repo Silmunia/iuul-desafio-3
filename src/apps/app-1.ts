04a41b342e651a3f91904f1b3894b7ba3b0c107f
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-04-12 16:58:51 -0300

    Refactor for Funcionario to have at least 1 Cargo

--- src/apps/app-1.ts
+++ src/apps/app-1.ts
@@ -3,11 +3,11 @@ import Cargo from "../objects/classes/Cargo";
 import Funcionario from "../objects/classes/Funcionario";
 
 let cargoAtendente = new Cargo("Atendente");
-let atendente = new Funcionario([cargoAtendente], "123.456.789-10", "Fun A", "(12) 3456-7890", 2500);
+let atendente = new Funcionario(cargoAtendente, [], "123.456.789-10", "Fun A", "(12) 3456-7890", 2500);
 console.log("Novo Funcionário criado:");
 console.log(atendente);
 
 let cargoGerente = new Cargo("Gerente");
-let gerente = new Funcionario([cargoGerente], "109.876.543-21", "Fun B", "(10) 9876-5432", 5000);
+let gerente = new Funcionario(cargoGerente, [], "109.876.543-21", "Fun B", "(10) 9876-5432", 5000);
 console.log("Novo Funcionário criado:");
 console.log(gerente);
--- src/objects/classes/Funcionario.ts
+++ src/objects/classes/Funcionario.ts
@@ -8,10 +8,11 @@ class Funcionario extends Pessoa implements IUsuario {
     public readonly cargos: Array<Cargo> = [];
     public salario: number;
 
-    constructor(cargos: Array<Cargo>, cpf: string, nome: string, telefone: string, salario: number) {
+    constructor(cargoInicial: Cargo, outrosCargos: Array<Cargo>, cpf: string, nome: string, telefone: string, salario: number) {
         super(cpf, nome, telefone);
 
-        this.cargos = cargos;
+        this.cargos.push(cargoInicial);
+        this.cargos.concat(outrosCargos);
         this.salario = salario;
 
         for (let i = 0; i < this.cargos.length; i++) {
